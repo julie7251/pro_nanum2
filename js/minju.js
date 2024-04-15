89000bbde20f75d6a6d59177dc408a95c470600e
minju0726 <minju0726@gmail.com>
2024-04-15 16:47:31 +0900

    민주

--- js/minju.js
+++ js/minju.js
@@ -1,1 +1,15 @@
 // 민주
+window.addEventListener("load", function () {
+  var swiper = new Swiper(".nanumsns", {
+    loop: true,
+    slidesPerView: 2.5,
+    spaceBetween: 0,
+    centeredSlides: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+    },
+  });
+
+
+
+});
