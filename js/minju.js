d102a3163ee07b2e56f79a429f7cf437f158d315
minju0726 <minju0726@gmail.com>
2024-04-30 15:52:22 +0900

    2안 로그인 회원가입

--- js/minju.js
+++ js/minju.js
@@ -34,7 +34,11 @@ window.addEventListener("load", function () {
     slidesPerView: 2,
     spaceBetween: 0,
     // centeredSlides: true,
+    
     breakpoints: {
+      769: {
+        slidesPerView: 1,
+      },
       768: {
         slidesPerView: 2,
       },
@@ -70,4 +74,20 @@ window.addEventListener("load", function () {
       topBtnImg.classList.remove("up");
     }
   });
+
+  var swiper = new Swiper(".nanumPcbanner", {
+    autoplay: {
+      delay: 2000, // 슬라이드 간격을 여기에 밀리초 단위로 설정합니다.
+    },
+    loop: true,
+    // slidesPerView: 4,
+    // spaceBetween: 0,
+    breakpoints: {
+      1300: {
+        slidesPerView:1,
+      },
+    },
+    // centeredSlides: true,
+  });
+
 });
